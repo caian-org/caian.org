f5c5c47ce8685d13fa7c5237c751a403f5ddf3b7
Caian R. Ertl <hi@caian.org>
2021-11-03 21:13:59 -0300

    build: improve gulp build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,54 +1,79 @@
-const { format: fmt } = require('util')
+/* standard */
 const { exec } = require('child_process')
 
+const { format } = require('util')
+const { basename, dirname, join } = require('path')
+
+/* 3rd-party */
 const pug = require('gulp-pug')
 const header = require('gulp-header')
-const { task, src, dest, series } = require('gulp')
+const flatmap = require('gulp-flatmap')
+const { task, series, src: from, dest: to } = require('gulp')
 
-/* ------------------------------------------------- */
+/* ................................................. */
 
-const d = (f) => fmt('./src/%s/**/*', f)
+const resolveDirs = () => {
+  const src = join(__dirname, 'www')
+  const dist = join(__dirname, 'dist')
 
-const run = (cmd, showOutput = true) =>
-  (callback) =>
-    exec(cmd, (err, stdout, stderr) => {
-      if (showOutput || err) {
-        console.log('\nstdout: \n' + stdout)
-        console.log('\nstderr: \n' + stderr)
-      }
+  return {
+    src,
+    dist,
+    intermediate: join(dist, 'intermediate'),
+    site: join(dist, 'site')
+  }
+}
 
-      callback(err)
-    })
+const p = resolveDirs()
+const sourceDir = (f) => format('%s/%s/**/*', p.src, f)
+const intermediateDir = (f) => format('%s/%s', p.intermediate, f)
 
-const deps = (...d) =>
-  series('copy:assets', 'copy:geocities', 'copy:blog', 'build:pug', ...d)
+/* ................................................. */
 
-/* ------------------------------------------------- */
+const yamlStart = ['---', '---', '', ''].join('\n')
+
+const run =
+  (cmd, showOutput = true) =>
+    (callback) =>
+      exec(cmd, (err, stdout, stderr) => {
+        if (showOutput || err) {
+          console.log('\nstdout: \n' + stdout)
+          console.log('\nstderr: \n' + stderr)
+        }
+
+        callback(err)
+      })
+
+const copyAll = (n) => from(sourceDir(n)).pipe(to(intermediateDir(n)))
+const deps = (...d) => series('build:pug', 'copy:assets', 'copy:geocities', 'copy:blog', ...d)
+
+/* ................................................. */
+
+task('copy:assets', () => copyAll('assets'))
+task('copy:geocities', () => copyAll('geocities'))
+task('copy:blog', () => copyAll('blog'))
 
 task('build:pug', () =>
-  src('./src/**/*.pug')
-    .pipe(pug({}))
-    .pipe(header(['---', '---', '', ''].join('\n')))
-    .pipe(dest('./intermediate')))
+  from(format('%s/**/*.pug', p.src))
+    .pipe(
+      flatmap((stream, file) => {
+        const htmlContent = stream.pipe(pug({}))
 
-task('copy:assets', () =>
-  src(d('assets'))
-    .pipe(dest('./intermediate/assets')))
+        switch (basename(dirname(file.path))) {
+          case '_includes':
+          case '_layouts':
+            return htmlContent
 
-task('copy:geocities', () =>
-  src(d('geocities'))
-    .pipe(dest('./intermediate/geocities')))
-
-task('copy:blog', () =>
-  src(d('blog'))
-    .pipe(dest('./intermediate/geocities')))
+          default:
+            return htmlContent.pipe(header(yamlStart))
+        }
+      })
+    )
+    .pipe(to(p.intermediate))
+)
 
 task('build:jekyll', run('bundle exec jekyll build', false))
-
 task('serve:jekyll', run('bundle exec jekyll serve'))
 
-/* ------------------------------------------------- */
-
 task('build', deps('build:jekyll'))
-
 task('default', deps('serve:jekyll'))
