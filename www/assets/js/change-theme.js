177d1596265003be6b86dbe2fb475e8d0a628434
Caian Ertl <hi@caian.org>
2021-11-07 05:49:50 -0300

    feat: greatly reduce the amount of code duplication

--- www/assets/js/change-theme.js
+++ www/assets/js/change-theme.js
@@ -1,13 +1,13 @@
-function changeTheme() {
+function changeTheme () {
   const c = '/assets/css'
 
   const theme = document.getElementById('theme')
   const syntax = document.getElementById('syntax')
-  const changeTheme = document.getElementById('change-theme')
+  const ct = document.getElementById('change-theme')
 
   const dim = theme.href.endsWith('bright.css') ? 'dark' : 'bright'
 
   theme.href = [c, 'default', dim].join('/').concat('.css')
   syntax.href = [c, 'syntax', dim].join('/').concat('.css')
-  changeTheme.innerHTML = (dim === 'bright' ? 'Dark' : 'Bright').concat(' theme')
+  ct.innerHTML = (dim === 'bright' ? 'Dark' : 'Bright').concat(' theme')
 }
